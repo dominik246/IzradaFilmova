6e9123aa99ae7157fe2a3e63ed99448fac4ae0c4
Dominik <dominik246@gmail.com>
2022-09-08 19:05:12 +0200

    Added dummy controllers

--- IzradaFilmova/Features/Director/Controller/DirectorController.cs
+++ IzradaFilmova/Features/Director/Controller/DirectorController.cs
@@ -1,4 +1,5 @@
-﻿using IzradaFilmova.Shared.APITemplate.Interfaces;
+﻿using IzradaFilmova.Shared.APITemplate.DTOs.DirectorController;
+using IzradaFilmova.Shared.APITemplate.Interfaces;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,5 +10,39 @@ namespace IzradaFilmova.Features.Director.Controller
     [ApiController]
     public class DirectorController : ControllerBase, IDirectorController
     {
+        public Task<IActionResult> CreateMovie(CreateMovieDTO newMovie, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DirectorAcceptSalaryRequest(AcceptSalaryRequestDTO acceptSalaryRequest, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> FetchAllMovies(MovieFilter? movieFilter, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetMovie(GetMovieDTO movieDto, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> InviteActor(InviteActorDTO inviteActor, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> StartOfFilming(StartFilmingDTO startFilming, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> StopOfFilming(StopFilmingDTO stopFilming, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- IzradaFilmova/Features/SuperAdmin/Controller/SuperAdminController.cs
+++ IzradaFilmova/Features/SuperAdmin/Controller/SuperAdminController.cs
@@ -12,5 +12,74 @@ namespace IzradaFilmova.Features.SuperAdmin.Controller
     [ApiController]
     public class SuperAdminController : ControllerBase, ISuperAdminController
     {
+        public Task<IActionResult> CreateActorAccount(NewActorAccountDTO newActorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> CreateDirectorAccount(NewDirectorAccountDTO newDirectorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> CreateGenre(CreateGenreDTO createGenre, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteActorAccount(DeleteActorAccountDTO deleteActorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteDirectorAccount(DeleteDirectorAccountDTO deleteDirectorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteGenres(DeleteGenreDTO deleteGenre, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> FetchActorAccounts(FetchActorAccountDTO fetchActorAccounts, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> FetchDirectorAccounts(FetchDirectorAccountDTO fetchDirectorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> FetchGenres(FetchGenreDTO fetchGenres, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetActorAccount(GetActorAccountDTO getActorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetDirectorAccount(GetDirectorAccountDTO getDirectorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateActorAccount(UpdateActorAccountDTO updateActorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateDirectorAccount(UpdateDirectorAccountDTO updateDirectorAccount, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateGenre(UpdateGenreDTO updateGenre, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
